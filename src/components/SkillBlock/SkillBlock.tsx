58d7b0e4a5df526292ba40fdf081c5b11e890525
Heloriel <andradematheus.dev@gmail.com>
2022-07-28 12:19:18 -0300

    Skills resposive layout update

--- src/components/SkillBlock/SkillBlock.tsx
+++ src/components/SkillBlock/SkillBlock.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export default function SkillBlock({ tech }: Props) {
   return (
-    <div className="flex w-24 h-24 justify-center items-center bg-nav-bg p-2 border border-violet-700 backdrop-blur-lg cursor-pointer">
+    <div className="flex  w-24 h-24 justify-center items-center bg-nav-bg p-2 backdrop-blur-lg cursor-pointer overflow-hidden rounded-xl">
       <img src={`/svgs/${tech}logo.svg`} className="w-10/12 hover:w-11/12 transition-all" />
     </div>
   );
--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -4,14 +4,17 @@ import SkillDescription from "../components/SkillDescription/SkillDescription";
 
 export default function Skills() {
   return (
-    <section id="skills" className="skills-section">
-      <div className="flex flex-1 flex-col items-center px-4 lg:px-16">
-        <SectionTitle title="Conhecimentos" className="my-20 text-xl" />
+    <section
+      id="skills"
+      className="skills-section overflow-hidden rounded-t-[48px] sm:rounded-none"
+    >
+      <div className="flex flex-1 flex-col items-center px-4 lg:px-10">
+        <SectionTitle title="Conhecimentos" className="my-16 text-xl" />
         <div className="flex flex-row flex-wrap justify-center lg:justify-between w-full">
-          <div className="w-full lg:w-2/5 mb-8 lg:mb-0">
+          <div className="w-full lg:pr-2 lg:w-2/5 mb-8 lg:mb-0">
             <SkillDescription />
           </div>
-          <div className="grid lg:grid-cols-5 grid-cols-3 gap-6 lg:w-2/5">
+          <div className="grid lg:grid-cols-5 grid-cols-3 gap-6 w-full place-items-center lg:place-items-end lg:w-3/5">
             <SkillBlock tech="html" />
             <SkillBlock tech="css" />
             <SkillBlock tech="sass" />
