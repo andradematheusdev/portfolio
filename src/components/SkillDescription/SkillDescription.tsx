0bff06f18e9418cb6be68b813c3de847a1d013b6
Heloriel Thrayn <helore.art@gmail.com>
2022-07-28 20:45:36 -0300

    Mobile performance fix

--- src/components/SkillDescription/SkillDescription.tsx
+++ src/components/SkillDescription/SkillDescription.tsx
@@ -1,6 +1,6 @@
 export default function SkillDescription() {
   return (
-    <div className="flex flex-col w-full bg-nav-bg rounded-lg p-8 backdrop-blur-md">
+    <div className="flex flex-col w-full bg-nav-bg rounded-lg p-8">
       <h2 className="text-violet-500 text-3xl font-bold mb-8">ReactJS</h2>
       <p className="text-white leading-relaxed text-justify">
         Loren ipsun dolor sit amet Loren ipsun dolor sit amet Loren ipsun dolor sit amet Loren ipsun
