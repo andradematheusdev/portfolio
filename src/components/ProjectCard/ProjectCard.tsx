ac25dc67110d24624eaeac29275b16e44257130a
Heloriel <andradematheus.dev@gmail.com>
2022-08-01 15:49:17 -0300

    Get projects from cms

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,56 +1,42 @@
 import { useEffect, useState } from "react";
 import Button from "../Button/Button";
 
-interface IDogApi {
-  message: string;
-}
+type Props = {
+  name: string;
+  description: string;
+  stack: string[];
+  demo: string;
+  image: {
+    url: string;
+  };
+};
 
-export default function ProjectCard() {
-  const [dog, setDog] = useState("");
-
-  function fetchDog<T>(url: string) {
-    return fetch(url).then((response) => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    });
-  }
-
-  useEffect(() => {
-    fetchDog<{ message: string }>("https://dog.ceo/api/breeds/image/random")
-      .then(({ message }) => {
-        setDog(message);
-      })
-      .catch((error) => {
-        return "";
-      });
-  }, []);
-
+export default function ProjectCard({ demo, description, name, stack, image }: Props) {
   return (
     <div className="flex flex-col w-full bg-zinc-800 rounded-xl overflow-hidden backdrop-blur-md">
       <div className="flex w-full h-64 overflow-hidden">
-        <img src={dog} className="w-full object-cover object-center" />
+        <img src={image.url} className="w-full object-cover object-center" />
       </div>
       <div className="flex w-full p-2 text-xl font-bold justify-center text-white py-4">
-        <span>Blog</span>
+        <span>{name}</span>
       </div>
       <div className="flex w-full px-6">
-        <span className="text-justify text-white text-md">
-          Desafio 01 da Codelândia onde a proposta era construir o front-end de um blog responsivo,
-          aproveitei a oportunidade para deixar o blog funcional e por em pratica meus conhecimentos
-          com GraphQL e GraphCMS.
-        </span>
+        <span className="text-justify text-white text-md">{description}</span>
       </div>
       <div className="flex w-full p-4 text-sm">
         <div className="flex flex-row border-t border-zinc-700 w-full pt-4 text-white">
           <div className="flex flex-row flex-1 items-center">
-            <img src="/svgs/reactlogo.svg" className="w-8 h-8 mr-4" />
-            <img src="/svgs/tailwindlogo.svg" className="w-8 h-8 mr-4" />
-            <img src="/svgs/graphqllogo.svg" className="w-8 h-8 mr-4" />
+            {stack.map((tech) => {
+              return <img src={`/svgs/${tech}logo.svg`} className="w-8 h-8 mr-4" />;
+            })}
           </div>
           <div className="flex flex-row items-center justify-center">
-            <Button text="DEMO" className="bg-violet-500 text-white rounded px-8" />
+            <Button
+              text="DEMO"
+              className="bg-violet-500 text-white rounded px-8"
+              url={demo}
+              blank
+            />
           </div>
         </div>
       </div>
--- src/sections/ProjectsSection.tsx
+++ src/sections/ProjectsSection.tsx
@@ -1,18 +1,71 @@
+import { gql, useQuery } from "@apollo/client";
 import ProjectCard from "../components/ProjectCard/ProjectCard";
 import SectionTitle from "../components/SectionTitle/SectionTitle";
 
+interface Projects {
+  id: string;
+  name: string;
+  description: string;
+  stack: string[];
+  demo: string;
+  image: {
+    url: string;
+  };
+}
+
 export default function ProjectsSection() {
+  const GET_PROJECTS_QUERY = gql`
+    query GetProjects {
+      projects(first: 50) {
+        demo
+        description
+        id
+        name
+        stack
+        image {
+          url
+        }
+      }
+    }
+  `;
+
+  const { data, loading } = useQuery<{ projects: Projects[] }>(GET_PROJECTS_QUERY);
+
+  if (loading) {
+    return (
+      <section id="projects" className="projects-section px-4 lg:px-10">
+        <SectionTitle title="projetos" className="text-lg my-16" />
+        <div className="flex w-full justify-center items-center">Carregando...</div>
+      </section>
+    );
+  }
+
+  console.log(data);
+
+  if (!data || !data.projects) {
+    return (
+      <div className="flex w-full min-h-screen">
+        <span>Ops... Não foi possivel carregar o conteúdo 😞</span>
+      </div>
+    );
+  }
+
   return (
-    <section id="projects" className="projects-section px-4 lg:px-24">
+    <section id="projects" className="projects-section px-4 lg:px-10">
       <SectionTitle title="projetos" className="text-lg my-16" />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {data.projects.map((project) => {
+          return (
+            <ProjectCard
+              demo={project.demo}
+              description={project.description}
+              key={project.id}
+              name={project.name}
+              stack={project.stack}
+              image={{ url: project.image.url }}
+            />
+          );
+        })}
       </div>
     </section>
   );
