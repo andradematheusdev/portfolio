1997423987f7d0adeba617a6f609f0cf38e6609f
Heloriel <andradematheus.dev@gmail.com>
2022-09-01 11:28:32 -0300

    Fix: contact form outline

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -13,7 +13,18 @@ export default function TextInput({className, placeHolder, type = "text", name, 
         name={name}
         id={id}
         className={
-          `block w-full border border-violet-500 bg-zinc-800 text-white rounded h-14 p-4 ${className}`
+          `
+          block 
+          w-full
+          border
+          border-violet-500 bg-zinc-800 text-white
+          rounded
+          h-14
+          p-4
+          focus:outline-none
+        focus:border-violet-800
+          ${className}
+          `
         }
         placeholder={placeHolder}
     />
--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -21,8 +21,18 @@ export default function TextArea({children, className, cols = 1, rows, noResize 
         placeholder={placeHolder}
         className={
             classNames(
-                `block w-full border border-violet-500 bg-zinc-800 text-white rounded p-4 ${className}`, {'resize-none': noResize}
-                )
+                `
+                block
+                w-full
+                border
+                border-violet-500 bg-zinc-800 text-white
+                rounded
+                p-4
+                focus:outline-none
+                focus:border-violet-800
+                ${className}`,
+                {'resize-none': noResize}
+            )
         }
     >
         {children}
