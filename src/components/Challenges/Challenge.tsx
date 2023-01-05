d2c9e55ec7064e2fc0480eaeb5c666bebf2c53a1
Heloriel <andradematheus.dev@gmail.com>
2023-01-05 11:21:02 -0300

    Challenge visual update base

--- src/components/Challenges/Challenge.tsx
+++ src/components/Challenges/Challenge.tsx
@@ -6,9 +6,16 @@ type Props = {
 
 export default function Challenge({ title, url, desc }: Props) {
   return (
-    <a href={url} className="flex flex-col flex-1 group" target={"_blank"}>
-      <span className="text-violet-500 font-bold text-lg">{title}</span>
-      <span className="text-white text-justify group-hover:underline">{desc}</span>
+    <a
+      href={url}
+      className="
+        flex flex-col flex-1 rounded-lg overflow-hidden items-center justify-center hover:scale-110 transition-transform
+        bg-[url('/images/bg.jpg')] bg-no-repeat bg-center bg-cover min-h-[150px]
+      "
+      target={"_blank"}
+    >
+      <span className="text-white font-bold text-lg">{title}</span>
+      <span className="text-white text-center">{desc}</span>
     </a>
   );
 }
