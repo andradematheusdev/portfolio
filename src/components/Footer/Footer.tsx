1d0121b04c37185783e1164f26e0a866ee0e6e49
Heloriel <andradematheus.dev@gmail.com>
2023-01-06 11:06:28 -0300

    Footer logo url change

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
     <footer className="flex w-full bg-zinc-800 px-8 border-t border-zinc-600">
       <div className="flex flex-col md:flex-row w-full py-8">
         <span className="flex flex-1 justify-center md:justify-start mb-6 md:mb-0">
-          <Logo className={"fill-violet-500"} />
+          <Logo className={"fill-violet-500"} url="#" />
         </span>
         <span className="flex flex-1 justify-center md:justify-end text-white">
           {currentYear} - Feito com <Coffee className="mx-2" size={24} /> por mim.
