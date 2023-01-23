c163ef44342750c732596dec8fa78410bb502fa5
Heloriel <andradematheus.dev@gmail.com>
2023-01-23 15:46:01 -0300

    Fix: Mobile menu context type

--- src/contexts/MobileMenuContext.tsx
+++ src/contexts/MobileMenuContext.tsx
@@ -4,16 +4,15 @@ interface IMeunuContextProps {
   children: React.ReactNode;
 };
 
-type MenuContext = {
+export type MenuContextType = {
   menuState: boolean;
   setMenuState: Function;
 };
 
-export const Context = createContext({} as MenuContext);
+export const Context = createContext({} as MenuContextType);
 
-export default function MenuContext({ children }: IMeunuContextProps) {
+export const MenuContext = ({ children }: IMeunuContextProps) => {
   const [menuState, setMenuState] = useState(false);
-  const menuStatePack = { menuState, setMenuState };
 
-  return <Context.Provider value={menuStatePack}>{children}</Context.Provider>;
+  return <Context.Provider value={{menuState, setMenuState}}>{children}</Context.Provider>;
 }
