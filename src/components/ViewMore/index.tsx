975849374b303f01c05501c744c291234d0bb95b
Heloriel <andradematheus.dev@gmail.com>
2023-01-24 08:46:25 -0300

    Fix: View more text wrap

--- src/components/ViewMore/index.tsx
+++ src/components/ViewMore/index.tsx
@@ -8,6 +8,6 @@ type Props = {
 
 export const ViewMore = ({url, blank = false, className}: Props) => {
   return (
-    <a href={url} target={blank ? '_blank' : '_self'} className={`flex flex-row items-center ${className}`}>Ver Mais <CaretRight /></a>
+    <a href={url} target={blank ? '_blank' : '_self'} className={`flex flex-row items-center whitespace-nowrap ${className}`}>Ver Mais <CaretRight /></a>
   )
 }
