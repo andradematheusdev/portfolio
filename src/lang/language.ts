490d6da5f02d714245dfd37e78ae24529dabc4d9
Heloriel <andradematheus.dev@gmail.com>
2023-05-12 08:19:31 -0300

    Age update

--- src/lang/language.ts
+++ src/lang/language.ts
@@ -3,13 +3,13 @@ export const translation: ITranslation = {
     wellcomeSection: {
       helloWorld: "Olá Mundo, me chamo Matheus Andrade!",
       frontEnd: "Desenvolvedor Front-End focado em JavaScript",
-      about: "Tenho 27 anos, sou apaixonado por tecnologia, estudo programação por conta própria a mais de 1 ano e tenho muita facilidade para aprender coisas novas!",
+      about: "Tenho 28 anos, sou apaixonado por tecnologia, estudo programação por conta própria a mais de 1 ano e tenho muita facilidade para aprender coisas novas!",
     },
     aboutMeSection: {
       hello: "Olá, eu sou o",
       developerWith: "Desenvolvedor front-end com",
       timeline: {
-        one: "Tenho 27 anos, sou de Magé - RJ, estudo programação por conta própria e estou me graduando em Sistemas para Internet.",
+        one: "Tenho 28 anos, sou de Magé - RJ, estudo programação por conta própria e estou me graduando em Sistemas para Internet.",
         two: "Minha jornada na programação começou em 2013 quando comecei a estudar programção web com desenvolvimento em php.",
         three: "Em 2015 comecei a trabalhar como Designer Gráfico.",
         four: "Alinhando a experiência de 8 anos como Designer com meus estudos de programação percebi que o front-end era a área certa para mim.",
@@ -56,13 +56,13 @@ export const translation: ITranslation = {
     wellcomeSection: {
       helloWorld: "Hello World, I am Matheus Andrade!",
       frontEnd: "Front-End Developer focused on JavaScript",
-      about: "I'm 27 years old, passionate about technology, I've been studying programming on my own for over a year and it is very easy for me to learn new things!",
+      about: "I'm 28 years old, passionate about technology, I've been studying programming on my own for over a year and it is very easy for me to learn new things!",
     },
     aboutMeSection: {
       hello: "Hello, I am",
       developerWith: "Front-end developer with",
       timeline: {
-        one: "I'm 27 years old, I'm from Magé - RJ, I study programming on my own and I'm graduating in Systems for the  Internet.",
+        one: "I'm 28 years old, I'm from Magé - RJ, I study programming on my own and I'm graduating in Systems for the  Internet.",
         two: "My programming journey started in 2013 when I started studying web development with php.",
         three: "In 2015 I started working as a Graphic Designer.",
         four: "Aligning the experience of 8 years as a Designer with my programming studies, I realized that front-end development was the right area for me.",
